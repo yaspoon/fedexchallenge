40bbb2a9ccb8671855365ef48fa1f36f5d4eb2dd
Brock York <twunkown@gmail.com>
2013-07-09 20:14:02 +0800

    Base class that all ojbects that need to collide will inherit from

--- js/CollisionBlock.js
+++ js/CollisionBlock.js
@@ -1,9 +1,11 @@
-function CollisionBlock(x, y, width, height)
+function CollisionBlock(x, y, width, height, moveable)
 {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.moveable = moveable;
+    
     
     
 }
